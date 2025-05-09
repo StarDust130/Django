70dbb8f66ad5914ff64f3db04f6a3089b71e7748
ChandraShekhar <csyadav0513@gmail.com>
2025-05-09 16:54:02 +0530

    Implement home view with template rendering and add index.html

--- eternity/eternity/views.py
+++ eternity/eternity/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello Eternity Home Page! 😆")
+    return render(request , "website/index.html")
 
 
 def about(request):
