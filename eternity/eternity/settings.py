7e4a6e39d1f04287a3eb0586ad2345cb9d191a0c
ChandraShekhar <csyadav0513@gmail.com>
2025-05-10 11:22:49 +0530

    Add NPM_BIN_PATH setting for Node.js integration

--- eternity/eternity/settings.py
+++ eternity/eternity/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
